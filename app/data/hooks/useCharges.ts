88d754a84b5cc54ac28a07dfc4c1e4f831c3375c
Gavin Gregory <gavin.a.gregory@gmail.com>
2024-03-11 12:34:44 -0700

    update fetch calls to use env variable instead of direct string to api url

--- app/data/hooks/useCharges.ts
+++ app/data/hooks/useCharges.ts
@@ -26,47 +26,56 @@ export type tempParsed = {
 };
 
 export const addCharge = async (data: Charge | Record<string, any>) => {
-  const response = await fetch("http://localhost:1337/api/charges", {
-    method: "POST",
-    mode: "cors",
-    cache: "no-cache",
-    credentials: "same-origin",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization:
-        "bearer e2b4afd2f66dcb3ecc1f6419f87f84c110812b66efc0d5f5adef803bbd67f0b5c02874c53e7cd5abca4ee17b638f4fecab5a44e9cb1f8811157c728473f70438c9b9bf0e55c47df5d2eaf351c2788224c3ebda4c1f61cb7915b7c72caa8154ed94e23b3b283993de4b73d26148e4e63179aa13e93e8e38d290f1acf6a45eda9c",
-    },
-    redirect: "follow",
-    referrerPolicy: "no-referrer",
-    body: JSON.stringify(data),
-  });
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/charges`,
+    {
+      method: "POST",
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "bearer e2b4afd2f66dcb3ecc1f6419f87f84c110812b66efc0d5f5adef803bbd67f0b5c02874c53e7cd5abca4ee17b638f4fecab5a44e9cb1f8811157c728473f70438c9b9bf0e55c47df5d2eaf351c2788224c3ebda4c1f61cb7915b7c72caa8154ed94e23b3b283993de4b73d26148e4e63179aa13e93e8e38d290f1acf6a45eda9c",
+      },
+      redirect: "follow",
+      referrerPolicy: "no-referrer",
+      body: JSON.stringify(data),
+    }
+  );
 
   return await response.json();
 };
 
 export const deleteCharge = async (id: number) => {
-  const response = await fetch(`http://localhost:1337/api/charges/${id}`, {
-    method: "DELETE",
-    mode: "cors",
-    credentials: "same-origin",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization:
-        "bearer e2b4afd2f66dcb3ecc1f6419f87f84c110812b66efc0d5f5adef803bbd67f0b5c02874c53e7cd5abca4ee17b638f4fecab5a44e9cb1f8811157c728473f70438c9b9bf0e55c47df5d2eaf351c2788224c3ebda4c1f61cb7915b7c72caa8154ed94e23b3b283993de4b73d26148e4e63179aa13e93e8e38d290f1acf6a45eda9c",
-    },
-  });
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/charges/${id}`,
+    {
+      method: "DELETE",
+      mode: "cors",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "bearer e2b4afd2f66dcb3ecc1f6419f87f84c110812b66efc0d5f5adef803bbd67f0b5c02874c53e7cd5abca4ee17b638f4fecab5a44e9cb1f8811157c728473f70438c9b9bf0e55c47df5d2eaf351c2788224c3ebda4c1f61cb7915b7c72caa8154ed94e23b3b283993de4b73d26148e4e63179aa13e93e8e38d290f1acf6a45eda9c",
+      },
+    }
+  );
 
   return await response.json();
 };
 
 export const getCharges = async () => {
-  const response = await fetch("http://localhost:1337/api/charges", {
-    method: "GET",
-    headers: {
-      Authorization:
-        "bearer e2b4afd2f66dcb3ecc1f6419f87f84c110812b66efc0d5f5adef803bbd67f0b5c02874c53e7cd5abca4ee17b638f4fecab5a44e9cb1f8811157c728473f70438c9b9bf0e55c47df5d2eaf351c2788224c3ebda4c1f61cb7915b7c72caa8154ed94e23b3b283993de4b73d26148e4e63179aa13e93e8e38d290f1acf6a45eda9c",
-    },
-  });
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/charges`,
+    {
+      method: "GET",
+      headers: {
+        Authorization:
+          "bearer e2b4afd2f66dcb3ecc1f6419f87f84c110812b66efc0d5f5adef803bbd67f0b5c02874c53e7cd5abca4ee17b638f4fecab5a44e9cb1f8811157c728473f70438c9b9bf0e55c47df5d2eaf351c2788224c3ebda4c1f61cb7915b7c72caa8154ed94e23b3b283993de4b73d26148e4e63179aa13e93e8e38d290f1acf6a45eda9c",
+      },
+    }
+  );
   const data = await response.json();
   return data.data;
 };
