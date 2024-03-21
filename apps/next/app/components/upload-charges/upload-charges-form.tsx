376f79170e9367a80670b8e9076f864e903290ba
Gavin Gregory <gavin.a.gregory@gmail.com>
2024-03-21 10:54:58 -0700

    move upload charges into modal, handle closing of modal async after promise settles

---
+++ apps/next/app/components/upload-charges/upload-charges-form.tsx
@@ -0,0 +1,88 @@
+import { Label } from "@radix-ui/react-label";
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectItem,
+} from "../../../components/ui/select";
+import { Button } from "../../../components/ui/button";
+import {
+  Charge,
+  ChargeCardType,
+} from "../../../data/api/documentation.schemas";
+import { Input } from "../../../components/ui/input";
+import Papa, { ParseResult } from "papaparse";
+import { useCallback, useState } from "react";
+import { useAddCharge } from "../../data/hooks/useCharges";
+import { Session } from "next-auth";
+
+type UploadChargesFormProps = {
+  session: Session;
+  setOpen: (open: boolean) => void;
+};
+
+const UploadChargesForm = ({ session, setOpen }: UploadChargesFormProps) => {
+  const [file, setFile] = useState<File | undefined>();
+  const [cardType, setCardType] = useState<ChargeCardType>();
+
+  const { mutateAsync } = useAddCharge();
+
+  const parse = useCallback(() => {
+    new Promise((resolve) => {
+      resolve(
+        Papa.parse(file as File, {
+          complete: function (results: ParseResult<Charge>) {
+            const charges = results.data;
+            charges.forEach((charge, index) => {
+              mutateAsync({
+                added_by: session.user.name,
+                date: charge[0],
+                description: charge[1],
+                amount: charge[2],
+                card_type: cardType,
+              });
+            });
+          },
+        })
+      );
+    }).then(() => {
+      setOpen(false);
+    });
+  }, [cardType, file, mutateAsync, session.user.name, setOpen]);
+
+  async function handleOnChange(
+    e: React.FormEvent<HTMLInputElement>
+  ): Promise<void> {
+    const target = e.target as HTMLInputElement & {
+      files: FileList;
+    };
+
+    setFile(target.files[0]);
+  }
+  return (
+    <>
+      <Input id="upload" type="file" onChange={handleOnChange} accept=".csv" />
+      <Select onValueChange={(e: ChargeCardType) => setCardType(e)}>
+        <SelectTrigger>
+          <SelectValue placeholder="Select a Card Type..." />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="amex">American Express</SelectItem>
+          <SelectItem value="chase" disabled>
+            Chase
+          </SelectItem>
+          <SelectItem value="alaska" disabled>
+            Alaska Airlines
+          </SelectItem>
+          <SelectItem value="american" disabled>
+            American Airlines
+          </SelectItem>
+        </SelectContent>
+      </Select>
+      <Button onClick={parse}>Upload</Button>
+    </>
+  );
+};
+
+export default UploadChargesForm;
--- apps/next/app/components/upload-charges/upload-charges.tsx
+++ apps/next/app/components/upload-charges/upload-charges.tsx
@@ -1,96 +1,33 @@
-import { useAddCharge } from "../../data/hooks/useCharges";
-import Papa, { ParseResult } from "papaparse";
-import { useCallback, useState } from "react";
-
-import { Input } from "../../../components/ui/input";
-import { Label } from "../../../components/ui/label";
-import Loading from "../../global/loading";
 import { useSession } from "next-auth/react";
-import {
-  Charge,
-  ChargeCardType,
-} from "../../../data/api/documentation.schemas";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "../../../components/ui/select";
 import { Button } from "../../../components/ui/button";
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogTitle,
+  DialogHeader,
+} from "../../../components/ui/dialog";
+import UploadChargesForm from "./upload-charges-form";
+import { useState } from "react";
 
 const UploadCharges = () => {
-  const [file, setFile] = useState<File | undefined>();
-  const [cardType, setCardType] = useState<ChargeCardType>();
-
-  const { mutateAsync, isPending } = useAddCharge();
   const { data: session } = useSession();
-
-  const parse = useCallback(() => {
-    Papa.parse(file as File, {
-      complete: function (results: ParseResult<Charge>) {
-        const charges = results.data;
-        charges.forEach((charge, index) => {
-          mutateAsync({
-            added_by: session.user.name,
-            date: charge[0],
-            description: charge[1],
-            amount: charge[2],
-            card_type: cardType,
-          });
-        });
-      },
-    });
-  }, [cardType, file, mutateAsync, session.user.name]);
-
-  async function handleOnChange(
-    e: React.FormEvent<HTMLInputElement>
-  ): Promise<void> {
-    const target = e.target as HTMLInputElement & {
-      files: FileList;
-    };
-
-    setFile(target.files[0]);
-  }
-
-  console.log(cardType);
+  const [open, setOpen] = useState(false);
 
   return (
     <div className="grid w-full max-w-sm items-center gap-1.5">
-      {isPending ? (
-        <div>
-          <Loading />
-        </div>
-      ) : (
-        session && (
-          <>
-            <Label htmlFor="picture">Upload some charges!</Label>
-            <Input
-              id="picture"
-              type="file"
-              onChange={handleOnChange}
-              accept=".csv"
-            />
-            <Select onValueChange={(e: ChargeCardType) => setCardType(e)}>
-              <SelectTrigger>
-                <SelectValue placeholder="Select a Card Type..." />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="amex">American Express</SelectItem>
-                <SelectItem value="chase" disabled>
-                  Chase
-                </SelectItem>
-                <SelectItem value="alaska" disabled>
-                  Alaska Airlines
-                </SelectItem>
-                <SelectItem value="american" disabled>
-                  American Airlines
-                </SelectItem>
-              </SelectContent>
-            </Select>
-            <Button onClick={parse}>Upload</Button>
-          </>
-        )
+      {session && (
+        <Dialog open={open} onOpenChange={setOpen}>
+          <DialogTrigger asChild>
+            <Button>Upload Charges</Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-md">
+            <DialogHeader>
+              <DialogTitle>Upload Charges</DialogTitle>
+            </DialogHeader>
+            <UploadChargesForm session={session} setOpen={setOpen} />
+          </DialogContent>
+        </Dialog>
       )}
     </div>
   );
