59181cf76557d8f714183ac8f15077267bd584f1
Gavin Gregory <gavin.a.gregory@gmail.com>
2024-03-25 13:06:13 +0800

    tweak sidebar layout slightly

--- apps/next/app/global/sidebar.tsx
+++ apps/next/app/global/sidebar.tsx
@@ -9,18 +9,14 @@ import SelectDates from "../components/select-dates/select-dates";
 const Sidebar = () => {
   const { data: session, status } = useSession();
   return (
-    <div className="min-w-[300px]">
-      <div className="flex flex-col gap-2 px-8">
-        <Cardholder size={128} weight="fill" />
-        <ul>
-          <li>
-            <a href="charts" className="font-semibold">
-              Charts
-            </a>
-          </li>
-        </ul>
-        <UploadCharges />
-        <SelectDates />
+    <div className="min-w-[300px] mx-4">
+      <Cardholder size={128} weight="fill" />
+      <div className="flex flex-col justify-between">
+        <div className="flex flex-col gap-2 mb-6">
+          <UploadCharges />
+          <h3>Select a Month and Year:</h3>
+          <SelectDates />
+        </div>
         {!session ? (
           <Button onClick={() => signIn()}>Login</Button>
         ) : (
